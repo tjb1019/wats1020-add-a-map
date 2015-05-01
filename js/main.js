deb149595a79c9ca0d1c3c2cdb2b65cd608888a0
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2015-05-01 04:34:18 -0400

    added styles

--- js/main.js
+++ js/main.js
@@ -44,15 +44,3 @@ $(document).ready(function() {
 	marker3.bindPopup("<b>Gibraltar Rock</b><br>Save yourself the trip to Spain.");
 
 });
-// TODO: Inside of your on ready handler, invoke the Leaflet.js library
-// to draw a map in your `#map-container` div.
-
-// TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
-// tiles served through the MapQuest CDN. Consult this example to set up
-// the map tiles layers:
-
-
-// TODO: Customize that Map to show markers with popups at no fewer than 3
-// interesting locations. (You'll need to figure out the latitude/longitude for
-// these locations using a mapping tool such as Google Maps.)
-
